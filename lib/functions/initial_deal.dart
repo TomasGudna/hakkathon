736ce3d26eda023895469c92ac01268dc14c87a4
HR Bodvarsson <hrbodvarsson@gmail.com>
2024-03-23 13:27:15 +0000

    inital deal
    v1

--- lib/functions/initial_deal.dart
+++ lib/functions/initial_deal.dart
@@ -24,4 +24,10 @@ import 'draw_card.dart';
 
 void InitialDeal(List<int> playerHand, List<int> houseHand, List<int> deck) {
 
+  List <int> deck = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+  List <int> playerHand.add(DrawCard(deck));
+  List <int> dealerHand.add(DrawCard(deck));
+  List <int> playerHand.add(DrawCard(deck));
+  List <int> dealerHand.add(DrawCard(deck));
+
 }
